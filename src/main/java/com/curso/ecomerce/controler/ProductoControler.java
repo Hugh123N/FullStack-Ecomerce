77eae4175ca4c2bac101cfc85dd8ddcdb81d50d4
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-09-20 17:06:18 -0500

    creacion funcionalidad guardar producto,lista,create,logger para la clase productoControler

--- src/main/java/com/curso/ecomerce/controler/ProductoControler.java
+++ src/main/java/com/curso/ecomerce/controler/ProductoControler.java
@@ -1,16 +1,42 @@
 package com.curso.ecomerce.controler;
 
+//importa todo el paquete de logger con => *
+import com.curso.ecomerce.model.Producto;
+import com.curso.ecomerce.model.Usuario;
+import com.curso.ecomerce.service.ProductoService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoControler {
+    // logger es para hacer prueba del guardado correcto por consola con INFO
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoControler.class);
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show(){
         return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";
+    }
+
+    //metodo que mapea la informacion que va guardar un producto
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("este es el objeto producto {}",producto);
+        Usuario u=new Usuario(1,"","","","","","","",null,null);
+        producto.setUsuario(u);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 
 }
--- src/main/java/com/curso/ecomerce/model/Producto.java
+++ src/main/java/com/curso/ecomerce/model/Producto.java
@@ -2,11 +2,9 @@ package com.curso.ecomerce.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+@ToString
 @Getter
 @Setter
 @AllArgsConstructor
