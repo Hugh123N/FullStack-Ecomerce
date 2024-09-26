0a103fcf171d4e096e6552433ce34fc418be01ae
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-09-25 20:25:22 -0500

    la cantidad y buscar producto en el home controller para añadir al carrito

--- src/main/java/com/curso/ecomerce/controler/HomeControler.java
+++ src/main/java/com/curso/ecomerce/controler/HomeControler.java
@@ -1,5 +1,7 @@
 package com.curso.ecomerce.controler;
 
+import com.curso.ecomerce.model.DetalleOrden;
+import com.curso.ecomerce.model.Orden;
 import com.curso.ecomerce.model.Producto;
 import com.curso.ecomerce.service.ProductoService;
 import lombok.NoArgsConstructor;
@@ -8,11 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,17 +20,19 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/")
 public class HomeControler {
-
     private final Logger log= LoggerFactory.getLogger(HomeControler.class);
-
     @Autowired
     private ProductoService productoService;
+
+    //para almacenar los detalles de la orden
+    List<DetalleOrden> detalles=new ArrayList<DetalleOrden>();
+    //almacena datos de la orden en general
+    Orden orden=new Orden();
 
     @GetMapping("")
     public String home(Model model){
         List<Producto> productos=productoService.findAll();
         model.addAttribute("productos",productos);
-
         return "usuario/home";
     }
 
@@ -45,8 +47,14 @@ public class HomeControler {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id,@RequestParam Integer cantidad){
+        DetalleOrden detalleOrden=new DetalleOrden();
+        Producto producto=new Producto();
+        double sumaTotal=0;
 
+        Optional<Producto> optionalProducto=productoService.get(id);
+        log.info("producto añadido {}",optionalProducto.get());
+        log.info("cantidad {}",cantidad);
 
         return "usuario/carrito";
     }
