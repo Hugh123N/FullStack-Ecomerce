453b4103d976759d690f49d5a0c16aac976e0f83
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-09-25 19:33:33 -0500

    mostrar informacion del producto de la pagina producto home

--- src/main/java/com/curso/ecomerce/controler/HomeControler.java
+++ src/main/java/com/curso/ecomerce/controler/HomeControler.java
@@ -3,18 +3,25 @@ package com.curso.ecomerce.controler;
 import com.curso.ecomerce.model.Producto;
 import com.curso.ecomerce.service.ProductoService;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @NoArgsConstructor
 @Controller
 @RequestMapping("/")
 public class HomeControler {
+
+    private final Logger log= LoggerFactory.getLogger(HomeControler.class);
+
     @Autowired
     private ProductoService productoService;
 
@@ -26,5 +33,14 @@ public class HomeControler {
         return "usuario/home";
     }
 
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id,Model model){
+        log.info("id enviado como parametro {}",id);
+        Producto producto=new Producto();
+        Optional<Producto> productoOptional=productoService.get(id);
+        producto=productoOptional.get();
+        model.addAttribute("producto",producto);
+        return "usuario/productohome";
+    }
 
 }
