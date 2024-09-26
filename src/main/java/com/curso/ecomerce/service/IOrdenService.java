79a5fc5734e2e03c63d2c4a14f954afc15b6db5a
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-09-26 18:55:31 -0500

    metodo para generar en numero de la orden

--- src/main/java/com/curso/ecomerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecomerce/service/IOrdenService.java
@@ -2,7 +2,12 @@ package com.curso.ecomerce.service;
 
 import com.curso.ecomerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
 
+    List<Orden> findAll();
     Orden save(Orden orden);
+
+
 }
