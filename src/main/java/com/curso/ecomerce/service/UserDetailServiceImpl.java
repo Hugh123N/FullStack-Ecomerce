dc164f9b0fc17cffaaf14d7388fafc823816c2ee
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-11-06 10:26:36 -0500

    implementacion de la dependecia spring security y la clase UserDetailServiceImpl

--- src/main/java/com/curso/ecomerce/service/UserDetailServiceImpl.java
+++ src/main/java/com/curso/ecomerce/service/UserDetailServiceImpl.java
@@ -1,2 +1,44 @@
-package com.curso.ecomerce.service;public class UserDetailServiceImpl {
+package com.curso.ecomerce.service;
+
+import com.curso.ecomerce.model.Usuario;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired //inyecta a esta clase
+    private IUsuarioService usuarioService;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    HttpSession session;
+
+    private Logger log= LoggerFactory.getLogger(UserDetailServiceImpl.class);
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("esto es el username");
+        Optional<Usuario> optionalUser=usuarioService.findByEmail(username);
+        if(optionalUser.isPresent()){
+            log.info("esto es el id del usuario {}",optionalUser.get().getId());
+            session.setAttribute("idUsuario", optionalUser.get().getId());
+            Usuario usuario=optionalUser.get();
+            return User.builder().username(usuario.getNombre())
+                    .password(passwordEncoder.encode(usuario.getPassword())).roles(usuario.getTipo()).build();
+        }else
+            throw new UsernameNotFoundException("Usuario no encontrado");
+    }
 }
